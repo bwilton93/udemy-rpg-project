7d398843bf9e40265858c0f9d1e4a4b7b3b26ae8
Barney <b.wilton1993@gmail.com>
2025-02-11 21:43:54 +0000

    Created xInput variable for player movement script obtained by GetAxisRaw

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float xInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,8 +12,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("Jump!");
-        }
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 }
