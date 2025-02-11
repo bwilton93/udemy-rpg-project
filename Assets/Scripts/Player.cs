894bb06aa204718ae9ade1ec24049c9973d48026
Barney <b.wilton1993@gmail.com>
2025-02-11 22:04:49 +0000

    Created physics based movement, both horizontal and jumping handled

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
     public float xInput;
+    public float moveSpeed;
+    public float jumpHeight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         HorizontalMovement();
+        if (Input.GetKeyDown(KeyCode.Space)){
+            JumpImpulse();
+        }
     }
 
-    void HorizontalMovement() {
+    void HorizontalMovement() 
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        Vector3 playerMovement = new Vector3(xInput, 0, 0);
-        transform.Translate(playerMovement * Time.deltaTime);
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+    }
+
+    void JumpImpulse() 
+    {
+        rb.linearVelocityY = jumpHeight;
     }
 }
